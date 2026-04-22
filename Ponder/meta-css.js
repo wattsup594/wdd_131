d614ca76189305ce848478ee2213b3ed21469d12
wattsk95 <wat21035@byui.edu>
2026-04-22 15:04:25 -0600

    js

---
+++ Ponder/meta-css.js
@@ -0,0 +1,25 @@
+const PI = 3.14;
+let radius = 3;
+
+let area = radius * radius * PI;
+
+console.log(area);
+
+
+const one = 1;
+const two = '2';
+let results = one * two
+console.log(results);
+
+//scope
+
+let course = "CSE131"; //global scope
+let student;
+if (true) {
+     student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
