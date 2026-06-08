5a52106fe7a68da38978f936b48358d178e2c157
wattsk95 <wat21035@byui.edu>
2026-06-08 14:50:41 -0600

    did the objects ponder

---
+++ Ponder/courses.js
@@ -0,0 +1,57 @@
+
+
+
+
+const aCourse = {
+    code: 'CSE121b',
+    name: 'Javascript Language',
+    logo: 'images/js-logo.png',
+    
+    sections: [
+        { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
+        { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}
+        ],
+        
+    enrollStudent: function (sectionNum) {
+        // find the right section...Array.findIndex will work here
+        const sectionIndex = this.sections.findIndex(
+          (section) => section.sectionNum == sectionNum
+        );
+        if (sectionIndex >= 0) {
+          this.sections[sectionIndex].enrolled++;
+          renderSections(this.sections);
+        }
+    }
+    
+            
+            
+};
+
+function sectionTemplate(section) {
+    return `<tr>
+      <td>${section.sectionNum}</td>
+      <td>${section.roomNum}</td>
+      <td>${section.enrolled}</td>
+      <td>${section.days}</td>
+      <td>${section.instructor}</td></tr>`
+}
+
+function renderSections(sections) {
+const html = sections.map(sectionTemplate);
+document.querySelector("#sections").innerHTML = html.join("");
+}
+
+renderSections(aCourse.sections);
+
+document.querySelector("#enrollForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.enrollStudent(sectionNum);
+});
+            
+
+//document.querySelector('#courseName').textContent = aCourse.name;
+//document.querySelector('#courseCode').textContent = aCourse.code;
+
+//aCourse.enrollStudent();
